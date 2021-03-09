170d2bfc8225733e136c6beee23a4fee93f2447f
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-09 15:11:16 +0100

    add gulpfile -> copy all php file,  convert tailwindcss to normal css, autoprefix it, clean it, uglify js scripts, and gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const postcss = require("gulp-postcss");
+const tailwindcss = require("tailwindcss");
+const cleanCSS = require("gulp-clean-css");
+const stripCssComments = require('gulp-strip-css-comments');
+
+// Copy All php files
+gulp.task("php", function () {
+  return new Promise(function (resolve, reject) {
+    gulp.src("./src/components/*.php").pipe(gulp.dest("public/components"));
+    gulp.src("./src/pages/*.php").pipe(gulp.dest("public/pages"));
+    gulp.src("./src/*.php").pipe(gulp.dest("public"));
+    resolve();
+  });
+});
+
+
+// TODO: i need to install 
+// Optimize Images
+
+// convert tailwindcss to normal css, autoprefix it, clean it
+gulp.task("tailwindcss", function () {
+  return new Promise(function (resolve, reject) {
+    gulp
+      .src("./src/styles/*.css")
+      .pipe(postcss([tailwindcss("./tailwind.config.js")]))
+      .pipe(autoprefixer())
+      .pipe(concat("main.css"))
+      .pipe(stripCssComments())
+      .pipe(cleanCSS({ compatibility: "ie8" }))
+      .pipe(gulp.dest("./public/styles/"));
+    resolve();
+  });
+});
+
+// uglify js scripts
+gulp.task("scripts", function () {
+  return new Promise(function (resolve, reject) {
+    gulp
+      .src("./src/assets/js/*.js")
+      .pipe(concat("main.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest("./public/assets/js"));
+    resolve();
+  });
+});
+gulp.task("default", ["php", "optImg", "tailwindcss", "scripts", "watch"]);
+
+gulp.task("watch", function () {
+  return new Promise(function (resolve, reject) {
+    gulp.watch("./src/styles/*.css"), gulp.series("tailwindcss");
+    gulp.watch("./src/assets/js/*.js", gulp.series("scripts"));
+    gulp.watch("./src/components/*.php", gulp.series("php"));
+    gulp.watch("./src/pages/*.php", gulp.series("php"));
+    gulp.watch("./src/*.php", gulp.series("php"));
+    resolve();
+  });
+});
