11feb4826ac5c5234c47222e3a296e6fd466b47e
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-09 15:52:30 +0100

    include all components

--- src/pages/main.php
+++ src/pages/main.php
@@ -1,1 +1,8 @@
-<!-- main page -->
+<?php
+    include '../components/header.php';
+    include '../components/sidebar.php';
+    include '../components/agent.php';
+    include '../components/features.php';
+    include '../components/story.php';
+    include '../components/gallery.php';
+    include '../components/footer.php';
