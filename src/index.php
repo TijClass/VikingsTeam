67908d36f6086781c342709332bcc8d72ceed491
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-09 15:52:07 +0100

    include main

--- src/index.php
+++ src/index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zoox</title>
+    <title>The future is for riders - Zoox</title>
+    <link rel="stylesheet" href="./styles/main.css" />
+    <link rel="icon" href="./assets/img/favicon.svg">
+
 </head>
+
 <body>
-    
+    <?php
+        include './pages/main.php';
+    ?>
+    <script src="./assets/js/script.js"></script>
 </body>
+
 </html>
