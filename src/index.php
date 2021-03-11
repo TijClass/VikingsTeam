391b0b4b04bec556c38c513ea96655b8fcab9c0a
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-11 09:52:18 +0100

    fix path

--- src/index.php
+++ src/index.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The future is for riders - Zoox</title>
-    <link rel="stylesheet" href="src/styles/main.css" />
-    <link rel="icon" href="src/assets/img/favicon.svg">
+    <link rel="stylesheet" href="public/styles/main.css" />
+    <link rel="icon" href="public/assets/img/favicon.svg">
 
 </head>
 
@@ -15,7 +15,8 @@
     <?php
     include './pages/main.php';
     ?>
-    <script src="src/assets/js/script.js"></script>
+    
+    <script src="public/assets/js/main.js"></script>
 </body>
 
 </html>
