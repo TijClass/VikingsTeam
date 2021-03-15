4f88e3e7bcfd97ba1dd13020edcef5758a31716b
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-15 11:54:07 +0100

    add new color to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        oceangreen: {
+          100: "#539F7D",
+        },
         "light-blue": colors.lightBlue,
         cyan: colors.cyan,
       },
