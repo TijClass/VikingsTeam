73658f642705e7405ef55d1ca2e22e917a387eb5
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-16 14:00:27 +0100

    add Montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
       colors: {
         oceangreen: {
           100: "#539F7D",
