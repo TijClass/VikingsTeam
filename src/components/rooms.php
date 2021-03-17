feaa5a74e5874981e2b7f7e046b2ce660b867318
abdellatif <ouldeabdellatif40@gmail.com>
2021-03-17 12:00:50 +0100

    add rooms

---
+++ src/components/rooms.php
@@ -0,0 +1,243 @@
+
+<section class=" container m-auto min-h-screen ">
+ <h1 class="text-center text-5xl">Our rooms</h1>
+ <p class=" w-8/12 text-center ml-44">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vel vitae iure qui dolorum maxime alias quibusdam distinctio dolorem dolor accusantium saepe .</p>
+  <div class=" h-full w-full mt-8 m-8 grid lg:grid-cols-5 gap-x-8   gap-4">
+    <!-- cartes go here / three 4 cartes-->
+
+     <!--ex-->
+      <div class=" w-full col-span-2 rounded overflow-hidden shadow-md	 flex flex-row relative">
+        <div class=" w-3/6 ">
+          <img src="https://source.unsplash.com/_AjqGGafofE/400x200" class="h-full w-full object-cover"  alt="couple">
+        </div>
+
+        <div class="  mx-3  w-3/6 flex flex-col justify-between  ">
+      <div>
+      <!--div for stars-->
+      <div class=" absolute top-4 ml-6 text-xs text-yellow-500">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      </div>
+      <!--end for stars-->
+       <h2 class=" font-normal ml-6 mt-9"> Hotel Blue Haven</h2>
+   <p class="text-left text-gray-600  text-sm px-6 py-6">
+When a hotel guest makes a reservation at your hotel, a quick text message to confirm their reservation will make guests feel confident in their reservations. You can also send out a message the day before your guest arrives.  </p>
+       <span  class=" font-thin  text-gray-600 ml-6 "><i class="fas fa-map-marker-alt">1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin  text-gray-600 ml-6 "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin  ml-6 text-gray-600 "><i class="fas fa-home">  1 * Room</i></span>
+      </div>
+
+       <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0 left-0 mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class="lg:absolute bottom-12">
+  <span class="   line-through ml-6 text-gray-600   ">$ 10,500</span>
+</div>
+
+ <div class="flex  justify-between  pb-2  "> 
+<h5 class="  font-normal  ml-4 text-4xl"> $ 8,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+
+        </div>
+      </div>
+
+      <!--ex-->
+
+   <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+
+
+   <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+
+ <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+   
+  
+
+   <!-- here 4 last cartes--> 
+
+ <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+
+    <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+
+   
+      <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+
+    <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+
+    <div class="  ml-2 mt-2 bg-white rounded overflow-hidden shadow-md relative">
+    <img src="https://source.unsplash.com/_AjqGGafofE/400x200" alt="couple">
+   <div class=" m-4 ">
+      <p class=" text-black font-bold   py-6">LUX * Belle Mare </p>
+      <span  class=" font-thin text-gray-600 text-base  "><i class="fas fa-map-marker-alt"> 1749 Wleeler Ridge Delaware</i></span>
+   <span class=" block  font-thin text-gray-600 text-base  "><i class="fas fa-user">   2 * Guests</i></span>
+   <span class=" block font-thin text-gray-600 text-base  "><i class="fas fa-home">  1 * Room</i></span>
+   
+   </div>
+   <div class=" bg-green-500 text-white text-sm font-bold normal-case rounded-r-lg p-2 absolute top-0  mt-4">
+  <span>Flash Offer</span>
+   </div>
+
+<div class=" absolute bottom-22 ">
+  <span class=" font-thin text-base text-gray-600  line-through ml-4 ">$ 8,500</span>
+</div>
+
+        <div class="flex flex-row justify-between lg:m-2 pt-6">
+<h5 class="  font-normal  text-4xl"> $ 5,500 </h5>
+<button type="submit" name="booking" class=" rounded bg-purple-900 text-white p-2 ">booking now</button>
+</div>
+   </div>
+   
+  
+  </div>
+</section>
