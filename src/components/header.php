f69c3454fe70accc4892465e1a1d79dde66c1293
allido808 <elghandour.hamza.all@gmail.com>
2021-03-18 00:44:42 +0100

    refactor(title 'h1'): responsive title for all devices
    for the  responsive i use 'sm','lg' and 'md' classes and a 66,667% of width for the container

--- src/components/header.php
+++ src/components/header.php
@@ -1,5 +1,22 @@
 <div class="min-h-screen bg-blue-100" style="background-image: url('https://a0.muscache.com/im/pictures/166791ff-bc82-4b88-ba3d-49be1d462dce.jpg?im_w=2560'); background-size: cover; background-repeat:no-repeat;background-position:center;">
   <div class="text-center text-white pt-8 pb-8 font-black">VikingsTeam.</div>
 
-  <hr class="w-10/12 text-white ml-auto mr-auto" />
+      <hr class="w-10/12 text-white ml-auto mr-auto" />
+
+  <div class="flex flex-row w-11/12 float-right mt-44">
+
+  <div class="w-4/6">
+      <h1 class="font-black text-white text-3xl sm:text-5xl lg:text-7xl">
+        Beautiful Places <br />
+        of Morocco
+      </h1>
+
+      <p class="mt-5 text-gray-300 font-medium sm:text-1xl lg:text-2xl">
+        Plan your vacation on the beautiful <br />
+        places of Morocco
+      </p>
+    </div>
+
+
+  </div>
 </div>
