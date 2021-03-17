cd1ba760b3ebdf62ad6e73fbb1dfbd68377b4000
allido808 <elghandour.hamza.all@gmail.com>
2021-03-17 14:06:37 +0100

    create header component

---
+++ src/components/header.php
@@ -0,0 +1,28 @@
+<div class="min-h-screen bg-blue-100 " style="background-image: url('https://a0.muscache.com/im/pictures/166791ff-bc82-4b88-ba3d-49be1d462dce.jpg?im_w=2560'); background-size: cover; background-repeat:no-repeat;background-position:center;">
+  
+  <div class="text-center text-white pt-8 pb-8 font-black"> VikingsTeam.</div>
+
+  <hr class=" w-10/12 text-white ml-auto mr-auto">
+
+  <div class="flex flex-row w-full mt-32">
+     <div class="w-4/6 ">
+       <h1 class="w-3/4  ml-auto mr-auto text-white not-italic font-black text-8xl tightest extra ff" style="font-family: Montserrat;"> Beautiful places <br>of Morocco</h1>
+       <p class="w-3/4 text-gray-300 mt-8 text-2xl ml-auto mr-auto tracking-wide ff text-3xl font-light not-italic opacity-80 "> Plan your vacation on the most beautiful <br> places of Morocco</p>
+     </div>
+     <div class="w-2/6 h-96 rounded-br-none rounded-tl-2xl pt-4 pl-6" style="background: rgba(255, 255, 255, 0.15);
+backdrop-filter: blur(70px);">
+         <div class=" w-3/6">
+           <p class=" text-lg " style="color: #FFFFFF;">up next</p>
+           <p class="text-white leading-loose font-medium text-4xl pt-4 pb-8">Boulvard <br> Maghreb <br> Arab</p>
+           <i class="fas fa-arrow-right " style="color: #FFFFFF;"></i>
+     
+         </div>
+         
+         <div class="w-3/6">
+            <img src="../assets/img/back.jpg" alt="">
+         </div>
+     
+     </div>
+  </div>
+
+</div>
