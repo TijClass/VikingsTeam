05556dbfc756e6ad6a7c7c18f48e62f2d9bad1dd
allido808 <elghandour.hamza.all@gmail.com>
2021-03-17 14:13:51 +0100

    modification on tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,28 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        oceangreen: {
-          100: "#539F7D",
+    theme: {
+        extend: {
+            lineHeight: {
+                'extra': '106px',
+            },
+            letterSpacing: {
+                'tightest': '0.01em',
+            },
+            fontfamily: {
+                'Montserrat': ['Montserrat', 'sans-serif']
+            },
+            colors: {
+
+                oceangreen: {
+                    100: "#539F7D",
+                },
+                "light-blue": 'colors.lightBlue',
+                'cyan': 'colors.cyan',
+                'gris': '#FFFFFF',
+            },
         },
-        "light-blue": colors.lightBlue,
-        cyan: colors.cyan,
-      },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins: [],
 };
