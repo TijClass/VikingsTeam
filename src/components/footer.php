d3aa640926c95fee559c62345a01241a6ab37d8b
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-03-18 10:21:56 +0100

    feat(components): done with the footer section
    create main site footer using grid for the footer( body, header -> UP) and flex for the footer ( buttom -> footer), the footer contain sitemap, social, head office also the newsletter
    
    PR Open #15

--- src/components/footer.php
+++ src/components/footer.php
@@ -49,7 +49,7 @@
                      Bacon ipsum dolor amet short ribs pig sausage prosciuto chicken spare ribs salami.
                  </p>
                  <div class="pt-12">
-                     <span class="text-base font-medium tracking-wide text-gray-300">Head Office</span>
+                     <span class="text-base font-medium tracking-wide text-gray-300">News letter</span>
                      <form class="flex flex-col mt-4 md:flex-row">
                          <input placeholder="Enter your email address" required="" type="text" class="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline" />
                          <button type="submit" class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white bg-oceangreen-100 transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">
