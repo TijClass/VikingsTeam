2a3939a1a3b7fadc1b927ab287d9ed65d0790978
abdellatif <ouldeabdellatif40@gmail.com>
2021-03-28 22:14:50 +0100

    feat(components) add booking section
    adding header and text and bottun and also images to booking section in the components but this section  needs some modifications

---
+++ src/components/story.php
@@ -0,0 +1,42 @@
+<div class=" bg-green-400  flex flex-row justify-around">
+    <div class="  px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="grid gap-10 lg:grid-cols-2">
+            <div class="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+                <div class="max-w-xl mb-6">
+                    <h2 class="max-w-lg mb-6 font-sans text-xs font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                    Lorem ipsum  <br> dolor sit amet
+                    </h2>
+                    <p class=" text-xs text-white md:text-lg">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                </div>
+                <div>
+                    <a href="/" class="inline-flex items-center justify-center h-12 px-5 mr-6 font-medium tracking-wide text-oceangreen-100 bg-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none ">
+                    Book Now
+                    </a>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+
+    <!--part of images-->
+   <div class=" lg:px-5 py-6 ">
+
+   <div class=" rounded-lg w-96 h-96 bg-gray-400 relative m-4 ">
+
+     <div class=" w-60 absolute top-14 right-44 border-8 border-gray-700 rounded-lg  " >
+        <img src="https://source.unsplash.com/_AjqGGafofE/400x200" class="border-4 rounded " >
+     </div>
+     <div class="  absolute top-36 left-16 border-8 border-gray-700 rounded-lg  " >
+        <img src="https://source.unsplash.com/_AjqGGafofE/400x200" class="border-4 rounded h-40 w-52 object-cover " >
+     </div>
+     <div class="  absolute left-44 lg:bottom-40 border-8 border-gray-700 rounded-lg  " >
+        <img src="https://source.unsplash.com/_AjqGGafofE/400x200" class="border-4 rounded h-40 w-36 object-cover " >
+     </div>
+
+     </div>
+   
+
+   </div>
+</div>
